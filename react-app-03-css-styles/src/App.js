a89f131f1085f87479254e192487bf2ef3892997
chetex <chetex69@gmail.com>
2023-10-21 17:32:38 +0200

    Added new css styles in index.js. Created new css file styles near app js.

---
+++ react-app-03-css-styles/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+/**
+ * LEARNING PATH
+ * 
+ * REACT-APP-03-CSS-STYLES
+ * 
+ *  > Each css for each component created.
+ *  > Take resources from github class. "index.css"
+ * 
+ */
---
+++ react-app-03-css-styles/src/Body.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Body = () => {
+    /**
+     * Gives capacity to change name request.
+     * @returns Array with names
+     */
+    const handleNameChange = () => {
+        const nameArray = ['Leo', 'Vega', 'Mama', 'Papa'];
+        const i = Math.floor(Math.random() * 4);
+
+        return nameArray[i];
+    }
+
+    return (
+        <body><div>Lista de ... {handleNameChange()}</div></body>
+    )
+}
+
+export default Body
