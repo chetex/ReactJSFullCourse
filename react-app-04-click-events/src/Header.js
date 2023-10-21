bf1d7aa29ea7fdbfd0cdd3918d1db9ee3f87abf5
chetex <chetex69@gmail.com>
2023-10-21 17:39:31 +0200

    changes over css

---
+++ react-app-04-click-events/src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+
+    /**
+     * Gives capacity to change name request.
+     * @returns Array with names
+     */
+    const handleShopNameChange = () => {
+        const shopArray = ['Carniceria list', 'Pescaderia list', 'Cosmeticos list'];
+        const i = Math.floor(Math.random() * 4);
+
+        return shopArray[i];
+    };
+
+    /**
+     * Assing function to the variable header Style.
+     */
+    const headerStyles = {
+        color: 'white'
+    };
+
+    return (
+        <header style={headerStyles}>
+            <h1>{handleShopNameChange()}</h1>
+        </header>
+    )
+}
+
+export default Header
